d702bb480c367c43b26f00e266e2755f83432b7a
“lxjjjjjj” <1732749825@qq.com>
2022-08-28 13:38:41 +0800

    修改

--- 响应式原理/index.js
+++ 响应式原理/index.js
@@ -6,23 +6,74 @@
 // 4.分支清除deps导致的无限循环effects执行
 // 5.无限调用effect执行 obj.foo = obj.foo + 1
 
-// computed的坑
+// computed注意的点
 // 1.lazy计算 effect的lazyoptions决定
 // 2.缓存值，在scheduler函数的执行后将dirty变成true重新计算取值
 // 3.当我们在另外一个effect中读取计算属性值的时候，
 //   它里面访问的响应式数据只会把computed内部的effect收集为依赖，
 //   外层effect不会被内层effect中的响应式数据收集
 
+
+// watch注意的点
+// 1.immediate 立即执行
+// 2.oldValue 和 newValue 的参数值
+// 3.watch函数是否要销毁
+
+// 代理Object要注意的点
+// 1.响应系统应拦截一切读取操作 包括有 访问属性obj.foo、判断对象或者原型上是否存在给定的key、使用for...in循环遍历对象 for(const key in obj){}
+// 2.
+
+
 // 什么是副作用函数，一个函数执行直接或间接影响其他函数执行
 // 在副作用函数中读取了某个响应式数据，响应式数据变化了，副作用函数要执行
 const obj = new Proxy(data, {
-    get(target, key){
+    get(target, key, receiver){
         track(target, key)
-        return target[key]
+        // 使用Reflect.get是为了避免一下这种更改this属性的情况
+        // const data = { // 实际调用get的时候 this是原始对象data 而不是 代理对象obj
+        //     foo: 1,
+        //     get bar(){
+        //         return this.foo
+        //     }
+        // }
+        return Reflect.get(target, key, receiver)
     },
-    set(target, key, newVal){
-        target[key] = newVal
-        trigger(target, key)
+    set(target, key, newVal,receiver){
+        // 如果属性不存在，说明是添加新属性，否则就是设置已有属性
+        const type = Object.prototype.hasOwnProperty.call(target, key) ? 'SET' : 'ADD'
+        const res = Reflect.set(target, key, newVal,receiver)
+        // 将 type 传给 trigger
+        trigger(target, key , type)
+        // 触发for...in的副作用执行
+        // trigger(target, ITERATE_KEY)
+        return res
+    },
+    // 拦截in操作符的读取
+    has(target, key){
+        track(target, key)
+        return Reflect.get(target, key)
+    },
+    // 拦截for...in操作的读取
+    // 当为target添加新的属性时，会对for...in循环产生影响，所以需要触发与ITERATE_KEY相关联的副作用函数重新执行
+    // 但是当为对象添加新属性的时候，也只是触发了这个属性的副作用函数重新执行，
+    // for...in循环是在副作用函数和ITERATE_KEY建立联系，这和新增加的属性一点关系也没有
+    // 解决方案就是 当添加属性的时候，我们将与ITERATE_KEY关联的副作用函数取出来执行就好了 在trigger函数内解决
+    ownKeys(target){
+        // ownkeys 用来获取target上所有的key 明显不和任何key绑定，所以我们自己构造唯一的key做为标识
+        track(target, ITERATE_KEY)
+        return Reflect.ownKeys(target)
+    },
+    deleteProperty(target, key){
+        // 检查被操作的属性是否是对象自己的属性
+        const hadKey = Object.prototype.hasOwnProperty.call(target,key)
+        const res = Reflect.defineProperty(target, key)
+
+        if(res && hadKey) {
+            // 只有当被删除的属性是自己的属性并且成功删除时 才触发更新
+            trigger(target, key, 'DELETE')
+        }
+
+        return res
     }
 })
 let activeEffect
@@ -98,10 +149,13 @@ function track(target, key) {
     deps.add(activeEffect)
     activeEffect.deps.push(deps)
 }
-function trigger(target, key){
+function trigger(target, key, type){
     const depsMap = bucket.get(target)
     if(!depsMap) return
+    // 取得与key关联的副作用函数
     const effects = depsMap.get(key)
+    // 取得与 ITERATE_KEY 相关联的副作用函数
+    const iterateEffects = depsMap.get(ITERATE_KEY)
     // 新建一个effects数组是为了effect执行前清除依赖 ，effect执行后重新添加依赖会导致循环不断进行
     const effectsToRun = new Set()
     // 避免循环执行
@@ -112,6 +166,15 @@ function trigger(target, key){
             effectsToRun.add(effectFn)
         }
     })
+    // 只有操作类型是新添加属性的时候才会将 ITERATE_KEY 相关联的副作用函数也添加到 effectsToRun 中
+    // 修改属性值，不需要重新执行for...in的副作用函数
+    if(type === 'ADD' || type === 'DELETE'){
+        iterateEffects && iterateEffects.forEach(effectFn => {
+            if(effectFn !== activeEffect){
+                effectsToRun.add(effectFn)
+            }
+        })
+    }
     effectsToRun.forEach(effectFn => {
         if(effectFn.options.scheduler){
             effectFn.options.scheduler(effectFn)
